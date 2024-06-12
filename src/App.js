f438a111b65b19e7d5fbd038e257b4753b960a2f
Parth <parthmmyadav@gmail.com>
2024-06-12 19:22:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
 import Counter from "./Counter";
 import Password from "./Password";
-import Leftpane from "./Leftpane";  // Assuming you have Leftpane component
-import Rightpane from "./Rightpane";
+
 
 function App() {
   return (
     <div className="min-h-screen grid grid-cols-3">
-      <Leftpane />
+      
       <div className="col-span-2 flex flex-col items-center">
         <Counter />
         <div className="h-20 flex"></div>
         <div className="text-xl">password generator</div>
         <Password />
       </div>
-      <Rightpane />
+      
     </div>
   );
 }
